6cdc784ab6d85d206c028cc257a4556f6c9ffab5
eurquhart1 <eu233@Emma-laptop>
2024-01-30 18:00:08 +0000

    Add implementation of montgomery multiplication

--- hw/ip/otbn/dv/otbnsim/test/test_montmul/template.s
+++ hw/ip/otbn/dv/otbnsim/test/test_montmul/template.s
@@ -1,15 +1,46 @@
 /* The input placeholders will be overwritten by the actual input values */
 
 .text
-  BN.ADDI    w10, w0, [inp1]    /* move input into r10 */
-  addi       x1, x0, 0x8000
-  addi       x3, x0, 11
-  BN.LID     x3, 0(x1)          /* now w11 contains qinv */
-  /* BN.MULQACC.Z  w2, w11.0, w10.0, 250   multiply the low word of w11 with the low word of w10, shift by 256-16 bits and store in w2 */  
+    /* Load input from memory */
+    la         x1, input
+    addi       x3, x0, 1
+    BN.LID     x3, 0(x1)          /*  w1 should now contain input */
 
-  ecall
+    /* Load QINV from memory */
+    la         x1, qinv
+    addi       x3, x0, 4
+    BN.LID     x3, 0(x1)          /*  w4 should now contain QINV */
+
+    /* Load 16-bit mask from memory */
+    la         x1, mask_16b
+    addi       x3, x0, 5
+    BN.LID     x3, 0(x1)          /*  w5 should now contain 16-bit mask */
+
+    BN.AND     w2, w5, w1         /*  w2 should contain low 16 bits of input */
+
+    BN.MULQACC.WO  w3, w2.0, w4.0, 0     /* w3 = (16_t)input * QINV */ 
+    ecall
 
 .data
-    .balign 32  /* Align to 4 bytes (32 bits) */
+    .balign 32
     qinv:
-    .word 0xfffff301  /* -3327 in 32-bit two's complement */
+    .word  0xf301  /* -3327 in 32-bit two's complement */
+    .word  0x0
+    .dword 0x0
+    .dword 0x0
+    .dword 0x0
+
+    .balign 32
+    mask_16b:
+    .word  0xffff  /* 16 set bits */
+    .word  0x0
+    .dword 0x0
+    .dword 0x0
+    .dword 0x0
+
+    .balign 32
+    input:
+    .dword  [inp1]
+    .dword 0x0
+    .dword 0x0
+    .dword 0x0
